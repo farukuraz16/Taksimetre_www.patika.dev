debfee2b2bb7b2618dbbe1d02f0d79e735d2ac8f
farukuzgun <farukuzgun@gmail.com>
2022-10-30 10:25:42 -0400

    patikaaaaaa

--- src/Taksimetre.java
+++ src/Taksimetre.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Taksimetre {
     public static void main(String[] args) {
         /*Taksimetre Programı
@@ -7,6 +9,16 @@ public class Taksimetre {
         Minimum ödenecek tutar 20 TL'dir. 20 TL altında ki ücretlerde yine 20 TL alınacaktır.
         Taksimetre açılış ücreti 10 TL'dir.
                  */
+
+        Scanner dat= new Scanner(System.in);
+        System.out.println("mesafe= ");
+        int yol= dat.nextInt();
+        double tutar=(yol*2.20)+10;
+        double odeme;
+        if (tutar>=20) odeme=tutar;
+        else odeme=20;
+
+        System.out.println("odeme = " + odeme);
 
 
     }
